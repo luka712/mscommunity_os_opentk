ed3c03be0cd1736f81f65e04b7b19323c61838d6
luka712 <lukaa712@hotmail.com>
2016-12-18 13:26:53 +0100

    fixed pipeline example

---
+++ Fixed_Pipeline/Fixed_Pipeline/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fixed_Pipeline
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using(var game = new Game())
+            {
+                game.Run(30.0);
+            }
+        }
+    }
+}
